b66adb9215d6abfc5c23750e2a96164eb7b93cc3
Richan_A27 <richanabraham2005@outlook.com>
2024-03-14 13:58:06 +0530

    exp-3 Perfect number

--- Experiment-3.py
+++ Experiment-3.py
@@ -1,14 +1,14 @@
 #Write a python program to check whether a number is perfect or not using a function.
 def perfect(num):
   l=[]
-  for i in range(num+1):
-    if i%num==0:
+  for i in range(1,num):
+    if (num%i==0):
       l.append(i)
+    
   if num==sum(l):
     print(f"{num} is a perfect number")
-
   else:
     print(f"{num} is not a perfect number")
+  print(l)
+perfect(5364)
 
-
-perfect(6)
